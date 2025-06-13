bc26aa1d4d62ddd22b67185570cafd5057f9cb5f
Musfirah-999 <zainabmusfirah.00@gmail.com>
2025-06-12 23:33:23 -0700

    ui

--- Bismillah/Bismillah/Program.cs
+++ Bismillah/Bismillah/Program.cs
@@ -1,3 +1,5 @@
+using Bismillah.UI;
+
 namespace Bismillah
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Bismillah
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Login());
+            Application.Run(new CashierDashboard());
         }
     }
 }
